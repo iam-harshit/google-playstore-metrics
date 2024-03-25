ea4ea0fcf610766ab20b83f5b1dd8aa966d0cc23
iam-harshit <importantemail545@gmail.com>
2024-03-25 20:36:36 +0530

    Implemented concise request body validation for user registration and login

--- Backend/src/validations/auth.validation.js
+++ Backend/src/validations/auth.validation.js
@@ -10,7 +10,7 @@ const {password} = require("./custom.validation");
 
 const register = {
     body: Joi.object().keys({
-        email: Joi.string().required.email(),
+        email: Joi.string().required().email(),
         password: Joi.string().required().custom(password),
         name: Joi.string().required()
     })
