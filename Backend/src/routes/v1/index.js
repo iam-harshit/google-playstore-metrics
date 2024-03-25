d8c79b866325f3622eb1229e34863704aafec6d8
iam-harshit <importantemail545@gmail.com>
2024-03-26 00:13:44 +0530

    Router configuration with authentication and metrics routes

--- Backend/src/routes/v1/index.js
+++ Backend/src/routes/v1/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const authRoute = require("./auth.route");
+const metricsRoute = require("./metrics.route");
 
 const router = express.Router();
 
-router.use("/auth",authRoute);
+router.use("/auth", authRoute);
+router.use("/metrics", metricsRoute);
 
 module.exports = router;
