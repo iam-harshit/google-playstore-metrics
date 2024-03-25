b6c47501a187b636b3bf72aebf339577c8d4645b
iam-harshit <importantemail545@gmail.com>
2024-03-25 20:33:10 +0530

    Defined password validation criteria

--- Backend/src/validations/custom.validation.js
+++ Backend/src/validations/custom.validation.js
@@ -2,7 +2,7 @@ const password = (value, helpers) => {
     if(value.length < 8){
         return helpers.message("Password must be at least 8 characters");
     }
-    if(!value.match(/\d/) || !value.match(/[a-ZA-Z]/)){
+    if(!value.match(/\d/) || !value.match(/[a-zA-Z]/)){
         return helpers.message(
             "Password must contain at least 1 letter and 1 number"
         );
