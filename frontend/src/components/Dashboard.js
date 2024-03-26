aeb1226dfc6792c7267624f26a9a6f655cf65306
iam-harshit <importantemail545@gmail.com>
2024-03-26 11:13:09 +0530

    Implemented Dashboard component with header and visualization container

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DashboardHeader from './DashboardHeader';
+import VisualizationContainer from './VisualizationContainer';
+
+function Dashboard() {
+  return (
+    <div>
+      <DashboardHeader />
+      <VisualizationContainer />
+    </div>
+  );
+}
+
+export default Dashboard;
